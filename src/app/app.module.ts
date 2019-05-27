cd1a0aedc1a4b79cae6bb4ba813d09d12a31d992
Training <mattia.artioli@student.unife.it>
2019-05-27 12:24:16 +0200

    booking VUOTO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,15 @@ import {AuthService} from './_services/auth-service.service';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModuleModule} from './routing-module/routing-module.module';
+import { BookingComponentComponent } from './booking-component/booking-component.component';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    BookingComponentComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
